2ba13a49fb376b258e0f8d607547ddb75b09ed83
MKchethan <90599627+MKchethan@users.noreply.github.com>
2022-10-10 13:04:42 +0530

    Update streamapp.py

--- streamapp.py
+++ streamapp.py
@@ -10,7 +10,8 @@ import ssl
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
-    passelse:
+    pass
+else:
     ssl._create_default_https_context = _create_unverified_https_context
 
 nltk.download()
