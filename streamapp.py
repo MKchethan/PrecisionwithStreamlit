4510d9e77e87661c39dd38e8c6e2955b51aac89d
MKchethan <90599627+MKchethan@users.noreply.github.com>
2022-10-10 13:16:27 +0530

    Update streamapp.py

--- streamapp.py
+++ streamapp.py
@@ -5,6 +5,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
+from ntlk.corpus import stopwords
+
 import ssl
 
 try:
